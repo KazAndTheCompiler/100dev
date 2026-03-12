d163ccc27d1ee403090a40c01c6398f27cfb9b6c
KazAndTheCompiler <kirkhenrik2@gmail.com>
2026-03-12 06:01:49 +0100

    commit all

---
+++ class20/main.js
@@ -0,0 +1,34 @@
+let name = "Henry";
+let age = 30;
+let likesCoffee = true;
+let     // makes a variable
+name    // the variable name
+=       // puts a value into it
+"Henry" // the value
+let city = "Kolding";
+console.log(city);
+let country = "Denmark";
+console.log(country);
+let continent = "Europe";
+console.log(continent);
+"hello"   // string
+42        // number
+true      // boolean
+let dog = "Max";
+let score = 100;
+let hungry = false;
+console.log(dog);
+console.log(score);
+console.log(hungry);
+let score = 10;
+score = 20;
+console.log(score); // 20
+let mood = "tired";
+mood = "awake";
+console.log(mood); // awake
+let points = 5; // create variable
+points = 6;     // change variable
+console.log(points); // 6
+let food = "pizza";
+food = "burger";
+console.log(food);
